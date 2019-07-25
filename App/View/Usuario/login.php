fc35d67643672e736a4d609577f5f4ecff19ac86
TopX <topx777@gmail.com>
2019-07-25 11:10:33 -0400

    Kevin

--- App/View/Usuario/login.php
+++ App/View/Usuario/login.php
@@ -58,32 +58,37 @@
 
   <div class="content content-fixed content-auth">
     <div class="container">
-      <div class="media align-items-stretch justify-content-center ht-100p pos-relative">
-        <div class="media-body align-items-center d-none d-lg-flex">
-          <div class="mx-wd-600">
-            <img src="https://via.placeholder.com/1260x950" class="img-fluid" alt="">
-          </div>
-        </div><!-- media-body -->
-        <div class="sign-wrapper mg-lg-l-50 mg-xl-l-60">
-          <div class="wd-100p">
-            <h3 class="tx-color-01 mg-b-5">Iniciar Sesión</h3>
-            <p class="tx-color-03 tx-16 mg-b-40">Bienvenido! Ingrese sus datos para continuar.</p>
+    <div class="media align-items-stretch justify-content-center ht-100p pos-relative">
+          <div class="media-body align-items-center d-none d-lg-flex">
+            <div class="mx-wd-600">
+              <img src="https://via.placeholder.com/1260x950" class="img-fluid" alt="">
+            </div>
+            
+          </div><!-- media-body -->
+          <div class="sign-wrapper mg-lg-l-50 mg-xl-l-60">
+            <div class="wd-100p">
+              <h3 class="tx-color-01 mg-b-5">Inicia Sesion</h3>
+              <p class="tx-color-03 tx-16 mg-b-40">Bienvenido porfavor inicia sesion para continuar.</p>
 
-            <div class="form-group">
-              <label>Correo</label>
-              <input type="email" class="form-control" placeholder="yourname@yourmail.com">
-            </div>
-            <div class="form-group">
-              <div class="d-flex justify-content-between mg-b-5">
-                <label class="mg-b-0-f">Contraseña</label>
-                <a href="" class="tx-13">Forgot password?</a>
+              <div class="form-group">
+                <label>Correo o Username</label>
+                <input type="email" class="form-control" placeholder="yourname@yourmail.com">
               </div>
-              <input type="password" class="form-control" placeholder="Ingrese su contraseña">
+              <div class="form-group">
+                <div class="d-flex justify-content-between mg-b-5">
+                  <label class="mg-b-0-f">Contraseña</label>
+                  <a href="" class="tx-13">olvidaste tu contraseña?</a>
+                </div>
+                <input type="password" class="form-control" placeholder="Enter your password">
+              </div>
+              <button class="btn btn-brand-02 btn-block">inicia Sesion</button>
+              <div class="divider-text">o</div>
+              <button class="btn btn-outline-facebook btn-block">inicia Sesion con Facebook</button>
+              <button class="btn btn-outline-twitter btn-block">inicia Sesion con Twitter</button>
+              <div class="tx-13 mg-t-20 tx-center">No tienes una cuenta? <a href="page-signup.html">Crear una cuenta</a></div>
             </div>
-            <button onclick="window.location.href='<?=RUTA_URL?>Usuario/Perfil'" class="btn btn-brand-02 btn-block">Ingresar</button>
-          </div>
-        </div><!-- sign-wrapper -->
-      </div><!-- media -->
+          </div><!-- sign-wrapper -->
+        </div><!-- media -->
     </div><!-- container -->
   </div><!-- content -->
 
