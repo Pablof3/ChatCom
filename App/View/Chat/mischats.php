58932c8b47ec6799587b403793c36c8c52001180
Pablof3 <PabloHy2898@hotmail.com>
2019-07-25 10:08:34 -0400

    arreglo vista

--- App/View/Chat/mischats.php
+++ App/View/Chat/mischats.php
@@ -70,13 +70,13 @@
 
             <div class="flex-fill pd-y-20 pd-x-10">
               <div class="d-flex align-items-center justify-content-between pd-x-10 mg-b-10">
-                <span class="tx-10 tx-uppercase tx-medium tx-color-03 tx-sans tx-spacing-1">All Channels</span>
+                <span class="tx-10 tx-uppercase tx-medium tx-color-03 tx-sans tx-spacing-1">Todos las areas</span>
                 <a href="#modalCreateChannel" class="chat-btn-add" data-toggle="modal"><span data-toggle="tooltip" title="Create Channel"><i data-feather="plus-circle"></i></span></a>
               </div>
               <nav id="allChannels" class="nav flex-column nav-chat mg-b-20">
                 <a href="#general" class="nav-link active"># general</a>
-                <a href="#engineering" class="nav-link"># engineering</a>
-                <a href="#products" class="nav-link"># products <span class="badge badge-danger">2</span></a>
+                <a href="#engineering" class="nav-link"># gerencia</a>
+                <a href="#products" class="nav-link"># atencio al cliente <span class="badge badge-danger">2</span></a>
               </nav>
             </div>
 
@@ -377,7 +377,7 @@
     </div>
   </div>
   <?php layout('Master/scripts')?>
-  <script src="../../assets/js/dashforge.chat.js"></script>
+  <script src="<?php asset('assets/js/dashforge.chat.js')?>"></script>
 </body>
 
 </html>
